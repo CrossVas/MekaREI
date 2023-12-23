dec518ef45e532e019e487acd159fda3e5616a4a
CrossVas <cross.wanted.1.4@gmail.com>
2023-12-23 15:27:25 +0900

    final touch before release

--- src/main/java/dev/crossvas/mekarei/handlers/FormulaicAssemblicatorTransferHandler.java
+++ src/main/java/dev/crossvas/mekarei/handlers/FormulaicAssemblicatorTransferHandler.java
@@ -14,6 +14,7 @@ import net.minecraft.world.inventory.Slot;
 
 import java.util.List;
 
+@SuppressWarnings("UnstableApiUsage")
 public class FormulaicAssemblicatorTransferHandler implements SimpleTransferHandler {
 
     @Override
